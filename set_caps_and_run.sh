e842b226fd68090503b716fc103effbdb46cf493
Esa Varemo <esa@kuivanto.fi>
2021-03-21 05:15:22 +0200

    Skip adding capabilities under cargo test

--- set_caps_and_run.sh
+++ set_caps_and_run.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -euo pipefail
 
-echo "Adding CAP_NET_ADMIN to '$1'"
-sudo setcap cap_net_bind_service=ep "$1"
+if ! ps aux | grep $(ps -o ppid= $$) | grep -q 'cargo test' ; then
+    echo "Adding CAP_NET_ADMIN to '$1'"
+    sudo setcap cap_net_bind_service=ep "$1"
+else
+    echo "Skipping capabilities due to running under test harness"
+fi
+
 echo "Executing '$1'"
 "$@"
